6809fb9215b7a3ed00fb2951897649ba195dde88
thành <vinhthanhluong@gmail.com>
2021-08-10 22:52:20 +0700

    update lan4

--- template/js/modules/SlideModule.js
+++ template/js/modules/SlideModule.js
@@ -11,27 +11,29 @@ export default function SlideModule(){
                     speed: 1000,
                     loop: false,
                     slidesPerView: 3,
-                    // autoHeight: true,
                     spaceBetween: 30,
-                    // autoplay: {
-                    //     delay: 4000,
-                    // },
                     pagination: {
                         el: '.swiper-pagination',
                         clickable: true,
                     },
-
-                    slidesPerColumnFill: 'row',
-                    slidesPerColumn: '2',
-                    // navigation: {
-                    //     nextEl: '.swiper-button-next',
-                    //     prevEl: '.swiper-button-prev',
-                    // },
-
-                   
-                    // observeParents:true,
-                    // observeSlideChildren: true,
-                    // observer: true,
+                    breakpoints: {
+                        320: {
+                            slidesPerView: 1,
+                            spaceBetween: 10,
+                        },
+                        768: {
+                            slidesPerView: 2,
+                            spaceBetween: 30,
+                            slidesPerColumnFill: 'row',
+                            slidesPerColumn: '2',
+                        },
+                        1200: {
+                            slidesPerView: 3,
+                            spaceBetween: 30,
+                            slidesPerColumnFill: 'row',
+                            slidesPerColumn: '2',
+                        }
+                    }
                 })
             }
             catch (err) {
@@ -54,20 +56,73 @@ export default function SlideModule(){
                 const swiper = new Swiper(sliderContainer, {
                     speed: 1000,
                     loop: false,
-                    slidesPerView: 6,
-                    allowTouchMove: false,
+                    
+                    
+                    pagination: {
+                        el: '.swiper-pagination',
+                        clickable: true,
+                    },
+                    // slidesPerColumnFill: 'row',
+                    // slidesPerColumn: '2',
+
+                    breakpoints: {
+                        320: {
+                            slidesPerView: 3,
+                            spaceBetween: 0,
+                            slidesPerColumnFill: 'row',
+                            slidesPerColumn: '2',
+                        },
+                        576: {
+                            slidesPerView: 4,
+                            slidesPerColumnFill: 'row',
+                            slidesPerColumn: '2',
+                        },
+                        768: {
+                            slidesPerView: 5,
+                            slidesPerColumnFill: 'row',
+                            slidesPerColumn: '2',
+                        },
+                        1024: {
+                            slidesPerView: 6,
+                            slidesPerColumnFill: 'row',
+                            slidesPerColumn: '2',
+                            allowTouchMove: false,
+                        }
+                    }
+                })
+            }
+            catch (err) {
+                console.log(err)
+            }
+
+        }
+    }
+
+    $('.slide-brand').length && slideBrand();
+
+    function slideTwo() {
+        if (document.querySelector('.slide-two')) {
+            const swiper = document.querySelector('.slide-two');
+            const sliderContainer = swiper.querySelector('.swiper-container');
+            const SliderPagination = swiper.querySelector('.swiper-pagination-orange');
+            const sliderPrevBtn = swiper.querySelector('.swiper-prev');
+            const sliderNextBtn = swiper.querySelector('.swiper-next');
+            try {
+                const swiper = new Swiper(sliderContainer, {
+                    speed: 1000,
+                    loop: true,
+                    slidesPerView: 2,
                     // autoHeight: true,
-                    // spaceBetween: 30,
-                    // autoplay: {
-                    //     delay: 4000,
-                    // },
+                    spaceBetween: 30,
+                    autoplay: {
+                        delay: 4000,
+                    },
                     pagination: {
                         el: '.swiper-pagination',
                         clickable: true,
                     },
 
-                    slidesPerColumnFill: 'row',
-                    slidesPerColumn: '2',
+                    
                     // navigation: {
                     //     nextEl: '.swiper-button-next',
                     //     prevEl: '.swiper-button-prev',
@@ -86,7 +141,51 @@ export default function SlideModule(){
         }
     }
 
-    $('.slide-brand').length && slideBrand();
+    $('.slide-two').length && slideTwo();
+
+    function slidePnew() {
+        if (document.querySelector('.slide-pnew')) {
+            const swiper = document.querySelector('.slide-pnew');
+            const sliderContainer = swiper.querySelector('.swiper-container');
+            const SliderPagination = swiper.querySelector('.swiper-pagination-orange');
+            const sliderPrevBtn = swiper.querySelector('.swiper-prev');
+            const sliderNextBtn = swiper.querySelector('.swiper-next');
+            try {
+                const swiper = new Swiper(sliderContainer, {
+                    speed: 1000,
+                    loop: false,
+                    slidesPerView: 2,
+                    // autoHeight: true,
+                    spaceBetween: 30,
+                    // autoplay: {
+                    //     delay: 4000,
+                    // },
+                    pagination: {
+                        el: '.swiper-pagination',
+                        clickable: true,
+                    },
+
+                    slidesPerColumnFill: 'row',
+                    slidesPerColumn: '3',
+                    // navigation: {
+                    //     nextEl: '.swiper-button-next',
+                    //     prevEl: '.swiper-button-prev',
+                    // },
+
+                   
+                    // observeParents:true,
+                    // observeSlideChildren: true,
+                    // observer: true,
+                })
+            }
+            catch (err) {
+                console.log(err)
+            }
+
+        }
+    }
+
+    $('.slide-pnew').length && slidePnew();
 
     if (document.querySelector('#slideasd')) {
         const swiper = document.querySelector('#slideasd');
